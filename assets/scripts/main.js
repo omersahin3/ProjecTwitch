72ecdf2c4500bbf08598411e56947d2a25448d29
omersahin3 <omersahine@hotmail.com>
2021-09-14 09:55:27 +0300

    some problems fixed

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,11 +4,11 @@ const s11 = document.querySelectorAll('#s11');
 const s212 = document.querySelectorAll('#s212');
 const s2111 = document.querySelectorAll('#s2111');
 const s3 = document.querySelectorAll('#s3');
-const side2 = document.getElementById('side-2');
+const side2 = document.getElementById('side2');
 const right = document.getElementById('right')
 function myFunction() {
-    s1.style.display = "none";
     side2.style.display = "none";
+    s1.style.display = "none";
 
     for(var i = 0; i < s11.length; i++) {
         s11[i].style.display = "none";
@@ -23,24 +23,22 @@ function myFunction() {
         s3[i].style.display = "none";
     }
     right.style.display = "inline-block";
-    sidebar.style.width="4%";
+    sidebar.style.minWidth="10px"
 }
 function myFunction2() {
-    s1.style.display = "flex";
     side2.style.display = "flex";
-
+    s1.style.display = "flex";
     for(var i = 0; i < s11.length; i++) {
         s11[i].style.display = "flex";
     }
     for(var i = 0; i < s2111.length; i++) {
-        s2111[i].style.display = "flex";
+        s2111[i].style.display = "block";
     }
     for(var i = 0; i < s212.length; i++) {
         s212[i].style.display = "flex";
     }
     for(var i = 0; i < s3.length; i++) {
-        s3[i].style.display = "flex";
+        s3[i].style.display = "block";
     }
-    right.style.display = "none";
-    sidebar.style.width="16%";
+    right.style.visibility = "hidden";
 }
